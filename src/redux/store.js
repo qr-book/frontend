f59f5b6fc044263d07b2ec3a4b7b6de83e4f15e5
JollyBanny <rootki7@protonmail.com>
2021-12-08 23:58:21 +1000

    add persist, rehydrate to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,41 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
 import rootReducer from "./reducers";
+
+// convert object to string and store in localStorage
+function saveToLocalStorage(state) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("persistantState", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+// load string from localStarage and convert into an Object
+// invalid output must be undefined
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("persistantState");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middlewares = [thunk];
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  loadFromLocalStorage(),
+  composeEnhancers(applyMiddleware(...middlewares))
 );
 
-window.store = store;
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
