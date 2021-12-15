21381281a0b53c00fd47f9f9fc9d7021cfc79394
JollyBanny <rootki7@protonmail.com>
2021-12-16 01:39:06 +1000

    created qrs api get and create

---
+++ src/service/api/qr.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import https from "https";
+
+const endpoints = {
+  get: (email, password, order) =>
+    axios.get("https:localhost/qr", {
+      auth: {
+        username: email,
+        password: password,
+      },
+      params: {
+        order: order,
+      },
+    }),
+  create: (
+    email,
+    password,
+    title,
+    text,
+    fgColor,
+    bgColor,
+    isURL,
+    frameType,
+    frameText,
+    frameBgColor,
+    frameTextColor,
+    quality
+  ) =>
+    axios.post(
+      "https:localhost/qr",
+      {
+        title: title,
+        text: text,
+        light_color: bgColor,
+        dark_color: fgColor,
+        isURL: isURL,
+        frame_id: frameType,
+        frame_text: frameText,
+        frame_color: frameBgColor,
+        frame_text_color: frameTextColor,
+        quality: quality,
+      },
+      {
+        auth: {
+          username: email,
+          password: password,
+        },
+      },
+      {
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      }
+    ),
+};
+
+export default endpoints;
