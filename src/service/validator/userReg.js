e433840b4663a36e105cac2789003f144ee858f9
JollyBanny <rootki7@protonmail.com>
2021-12-16 01:38:26 +1000

    update userReg schema validator

--- src/service/validator/userReg.js
+++ src/service/validator/userReg.js
@@ -1,8 +1,8 @@
 import * as yup from "yup";
 
 const schema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().min(6).required(),
+  email: yup.string().email("Invalid email").required("Please input login"),
+  password: yup.string().min(8, "Min password sybols is 8").required("Please input password"),
 });
 
 export default schema;
