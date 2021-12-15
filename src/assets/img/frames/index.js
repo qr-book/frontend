3d07eaea01211e6d8ec4fafed5822304468bdf23
JollyBanny <rootki7@protonmail.com>
2021-12-15 21:58:02 +1000

    added frames images

---
+++ src/assets/img/frames/index.js
@@ -0,0 +1,4 @@
+export { default as frame1 } from "./frame1.png";
+export { default as frame2 } from "./frame2.png";
+export { default as frame3 } from "./frame3.png";
+export { default as frame4 } from "./frame4.png";
