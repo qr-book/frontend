4ff4e4b79c5b0fc34f16064a08aa10632d6cfd47
JollyBanny <rootki7@protonmail.com>
2021-12-15 21:56:38 +1000

    completed profileEdit page

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -1,12 +1,55 @@
 import React from "react";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { userEdit } from "../service/validator";
+import api from "../service/api";
+import { useSelector, useDispatch } from "react-redux";
+import { setEmail, setName, logoutUser } from "../redux/actions/user";
+
 import defaultAvatarPng from "../assets/img/default_avatar.png";
 
 function ProfileEdit() {
+  const { lastEmail, password, name } = useSelector(({ user }) => {
+    return {
+      lastEmail: user.email,
+      password: user.password,
+      name: user.name,
+    };
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    clearErrors,
+  } = useForm({
+    resolver: yupResolver(userEdit),
+  });
+
+  const dispatch = useDispatch();
+  const onSubmit = async ({ email, name }) => {
+    try {
+      await api.user.edit(email, name, lastEmail, password);
+      dispatch(setName(name));
+      dispatch(setEmail(email));
+      window.location.replace("/profile");
+    } catch (e) {
+      if (e.response.status === 401) {
+        setError("data", {
+          type: "manual",
+          message: e.response.data.data,
+        });
+        dispatch(logoutUser());
+      }
+    }
+  };
+
   return (
     <div className="main">
       <div className="container">
-        <form className="profile-form">
+        <form onSubmit={handleSubmit(onSubmit)} className="profile-form">
           <div className="form-avatar">
             <img src={defaultAvatarPng} alt="" />
             <label htmlFor="avatar">
@@ -20,20 +63,38 @@ function ProfileEdit() {
             </label>
           </div>
           <div className="form-info">
+            <p className="error-msg">
+              {errors.email?.message ||
+                errors.password?.message ||
+                (errors.data ? errors.data.message : null)}
+            </p>
             <input
               type="text"
               name="nickname"
+              defaultValue={name}
               placeholder="nickname"
-              required
+              {...register("name", {
+                required: true,
+                onChange: () => {
+                  clearErrors("data");
+                },
+              })}
             />
             <input
               type="text"
               name="email"
+              defaultValue={lastEmail}
               placeholder="example@qr.com"
-              required
+              {...register("email", {
+                onChange: () => {
+                  clearErrors("data");
+                },
+              })}
             />
           </div>
-          <button className="link submit">Save</button>
+          <button type="submit" className="link submit">
+            Save
+          </button>
         </form>
       </div>
     </div>
