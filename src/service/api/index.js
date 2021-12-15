8286737694a7476031e29847c5f63e73eb319301
JollyBanny <rootki7@protonmail.com>
2021-12-15 21:55:12 +1000

    created qr api and renamed auth to  user api

--- src/service/api/index.js
+++ src/service/api/index.js
@@ -1,7 +1,9 @@
-import auth from "./auth";
+import user from "./user";
+import qr from "./qr";
 
 const allEndpoints = {
-  auth,
+  user,
+  qr,
 };
 
 export default allEndpoints;
--- src/service/api/user.js
+++ src/service/api/user.js
@@ -23,6 +23,25 @@ const endpoints = {
       password: password,
       name: email.split("@")[0],
     }),
+  edit: (email, nickname, lastEmail, password) =>
+    axios.put(
+      "https:localhost/user",
+      {
+        email: email,
+        name: nickname,
+      },
+      {
+        auth: {
+          username: lastEmail,
+          password: password,
+        },
+      },
+      {
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      }
+    ),
 };
 
 export default endpoints;
