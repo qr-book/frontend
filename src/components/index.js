13e794a6a688560932cfce4ef029ec302632665a
JollyBanny <rootki7@protonmail.com>
2021-12-17 17:46:00 +1000

    added qr load block

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ export { default as MenuPopup } from "./MenuPopup";
 export { default as MobileMenuPopup } from "./mobile/MobileMenuPopup";
 export { default as MobileSortPopup } from "./mobile/MobileSortPopup";
 export { default as QRBlock } from "./QRBlock/QRBlock";
+export { default as QRLoadBlock } from "./QRBlock/QRLoadBlock";
 export { default as QRBlockAdmin } from "./QRBlock/QRBlockAdmin";
 export { default as QRBlockMinimize } from "./QRBlock/QRBlockMinimize";
 export { default as Sort } from "./Sort";
